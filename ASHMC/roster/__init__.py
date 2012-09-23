758daef341c92e78d8ef5c572cc38a17628ee3b9
Haak Saxberg <haak.erling@gmail.com>
2012-09-22 19:10:37 -0700

    more logging things

--- ASHMC/roster/__init__.py
+++ ASHMC/roster/__init__.py
@@ -1,11 +1,16 @@
 from django.conf import settings
+from django.core.exceptions import MultipleObjectsReturned
+from django.db.models import ObjectDoesNotExist
 
 from django_auth_ldap.backend import populate_user
 
 from ASHMC.main.models import Campus, GradYear, Semester, Student
 from .models import Dorm, DormRoom, UserRoom
 
+import logging
 import xlrd
+
+logger = logging.getLogger(__name__)
 
 
 def create_user_related_things(*args, **kwargs):
@@ -16,18 +21,18 @@ def create_user_related_things(*args, **kwargs):
     print new_user, new_user.email
     # don't repopulate if we've already done the dance.
     if Student.objects.filter(user=new_user).count():
+        logger.debug("skipping student {}".format(new_user))
         return
 
-    if settings.DEBUG:
-        print "Creating user-related things..."
+    logger.info("new student {}".format(new_user))
+
     try:
         this_sem = Semester.get_this_semester()
         workbook = xlrd.open_workbook(settings.ROSTER_DIRECTORY + "{}.xlsx".format(
             this_sem.verbose_unicode()
         ))
     except IOError:
-        if settings.DEBUG:
-            print "Couldn't open workbook {}".format(this_sem.verbose_unicode())
+        logger.error("could not open workbook {}".format(this_sem.verbose_unicode()))
         return
 
     s = workbook.sheets()[0]
@@ -51,57 +56,67 @@ def create_user_related_things(*args, **kwargs):
         'JR': senior + 1,
         'SR': senior,
     }
+    try:
+        for r in xrange(ROSTER_ROW_START, s.nrows):
+            row = s.row(r)
 
-    for r in xrange(ROSTER_ROW_START, s.nrows):
-        row = s.row(r)
+            if row[FIELD_ORDERING.index('Email')].value != new_user.email:
+                logger.debug("\tSkipping row", r, row[FIELD_ORDERING.index('Email')].value)
+                continue
 
-        if row[FIELD_ORDERING.index('Email')].value != new_user.email:
-            if settings.DEBUG:
-                print "\tSkipping row", r, row[FIELD_ORDERING.index('Email')].value
-            continue
+            logger.debug("creating user-related things...")
+            year = CLASS_TO_GRADYEAR[row[FIELD_ORDERING.index('Class')].value]
 
-        if settings.DEBUG:
-            print "\tFound relevant row."
-
-        year = CLASS_TO_GRADYEAR[row[FIELD_ORDERING.index('Class')].value]
-
-        gradyear, _ = GradYear.objects.get_or_create(
-            year=year,
-        )
-
-        hmc = Campus.objects.get(code='HM')
-
-        studentid = None
-        new_student, _ = Student.objects.get_or_create(
-            user=new_user,
-            class_of=gradyear,
-            at=hmc,
-            studentid=studentid,
-            phonenumber=row[FIELD_ORDERING.index('Phone')].value,
-            #birthdate=datetime.datetime.strptime(row[FIELD_ORDERING.index('Birthdate')].value, '%b %d, %Y').date(),
-        )
-
-        dormname = row[FIELD_ORDERING.index('Dorm')].value.split(' ')
-
-        if dormname[0] != '':
-            if dormname[0] == "CGU":
-                dormname = ' '.join(dormname)
-            else:
-                dormname = dormname[0]
-
-            dorm = Dorm.objects.get(name__startswith=dormname)
-
-            room, _ = DormRoom.objects.get_or_create(
-                dorm=dorm,
-                number=row[FIELD_ORDERING.index("Room")].value,
+            gradyear, _ = GradYear.objects.get_or_create(
+                year=year,
             )
 
-            ur, _ = UserRoom.objects.get_or_create(
+            hmc = Campus.objects.get(code='HM')
+
+            studentid = None
+            new_student, _ = Student.objects.get_or_create(
                 user=new_user,
-                room=room,
+                class_of=gradyear,
+                at=hmc,
+                studentid=studentid,
+                phonenumber=row[FIELD_ORDERING.index('Phone')].value,
+                #birthdate=datetime.datetime.strptime(row[FIELD_ORDERING.index('Birthdate')].value, '%b %d, %Y').date(),
             )
 
-            ur.semesters.add(this_sem)
+            dormcode = row[FIELD_ORDERING.index('Dorm')].value.split(' ')
 
+            if dormcode[0] != '':
+                if dormcode[0] == "CGU":
+                    dormcode = ' '.join(dormcode)
+                else:
+                    dormcode = dormcode[0]
+
+                try:
+                    dorm = Dorm.objects.get(code__iexact=dormcode)
+                except ObjectDoesNotExist:
+                    try:
+                        dorm = Dorm.objects.get(name__startswith=dormcode)
+                    except (ObjectDoesNotExist, MultipleObjectsReturned):
+                        logger.error(
+                            "ldap population failed for {} (on failed dorm lookup {})".format(new_user, dormcode)
+                        )
+                        return
+
+                room, _ = DormRoom.objects.get_or_create(
+                    dorm=dorm,
+                    number=row[FIELD_ORDERING.index("Room")].value,
+                )
+
+                ur, _ = UserRoom.objects.get_or_create(
+                    user=new_user,
+                    room=room,
+                )
+
+                ur.semesters.add(this_sem)
+
+            return
+    except Exception, e:
+        # Don't interrupt core service, no matter what.
+        logger.error("caught unhandled exception {}: {}".format(e, e.message))
         return
 populate_user.connect(create_user_related_things)
